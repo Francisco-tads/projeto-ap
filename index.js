8d8cc67a97ebd37d34e68c0466897c9c74672c94
Francisco vieira <francisco.vieiratads@gmail.com>
2022-10-08 17:45:36 -0300

    going up one index.php

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
+express()
+
 app.use(express.static(path.join(__dirname, "assets")));
-
-
-
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // ROTA
 app.get("/", (req, res) => {
-    res.render(__dirname + 'index.html');
+    res.render('index.html');
 });
 
 
